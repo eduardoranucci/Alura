8a7d937fb48c52a4de1f6bfd441188d73fa8ac2b
Eduardo Ranucci <ffreakEd@outlook.com>
2021-12-24 16:53:59 -0300

    Update advinhacao.py

--- Python/1 - Python3 Pt. 1/advinhacao.py
+++ Python/1 - Python3 Pt. 1/advinhacao.py
@@ -4,15 +4,27 @@ cores = {
     'reset':'\033[m',
     'vermelho':'\033[0;31m',
     'vermelho_erro':'\033[1;31m',
-    'verde':'\033[1;32m',
+    'verde':'\033[0;32m',
+    'verde_negrito':'\033[1;32m',
     'sublinhado':'\033[4;37m',
-    'azul':'\033[0;36m'
+    'azul':'\033[0;36m',
+    'amarelo': '\033[0;33m'
 }
 
 print('-~-'*10, f'-~-  {cores["azul"]}Jogo da Adivinhação!{cores["reset"]}  -~-', '-~-'*10, sep='\n', end='\n\n')
 
+print('Qual nível de dificuldade?', f'\n\n{cores["verde"]}(1) Fácil {cores["amarelo"]}(2) Médio {cores["vermelho"]}(3) Difícil{cores["reset"]}', end='\n\n')
+
+nivel = int(input('Defina o nível: '))
+print('\n', end='')
+
+if nivel == 1: tentativas = 20
+elif nivel == 2: tentativas = 10
+else: tentativas = 5
+
 numero_secreto = randint(0, 100)
-tentativas = 3
+
+pontos = 1000
 
 for rodada in range(1, tentativas + 1):
     
@@ -31,12 +43,15 @@ for rodada in range(1, tentativas + 1):
 
     if acertou: 
         
-        print(f'\n{cores["verde"]}Você acertou!{cores["reset"]}', end='\n\n')
+        print(f'\n{cores["verde_negrito"]}Você acertou!{cores["reset"]} e fez {pontos}!', end='\n\n')
         break
 
     else: 
-        if maior: print(f'\n{cores["vermelho"]}Você errou!{cores["reset"]} Seu chute foi maior que o {cores["sublinhado"]}número secreto.{cores["reset"]}', end='\n\n')
-        if menor: print(f'\n{cores["vermelho"]}Você errou!{cores["reset"]} Seu chute foi menor que o {cores["sublinhado"]}número secreto.{cores["reset"]}', end='\n\n')
+        if maior: print(f'\n{cores["vermelho"]}Você errou!{cores["reset"]} Seu chute foi MAIOR que o {cores["sublinhado"]}número secreto.{cores["reset"]}', end='\n\n')
+        elif menor: print(f'\n{cores["vermelho"]}Você errou!{cores["reset"]} Seu chute foi MENOR que o {cores["sublinhado"]}número secreto.{cores["reset"]}', end='\n\n')
+        
+        pontos_perdidos = abs(numero_secreto - chute_jogador)
+        pontos -= pontos_perdidos
         
     
 print('-~-'*10, f'-~-~-~-  {cores["azul"]}Fim de jogo!{cores["reset"]}  -~-~-~-', '-~-'*10, sep='\n')
