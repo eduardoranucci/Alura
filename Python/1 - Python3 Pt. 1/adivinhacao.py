3beb3c95e8617772e729888c2bb6d799606719bd
Eduardo Ranucci <ffreakEd@outlook.com>
2021-12-25 17:57:29 -0300

    Update adivinhacao.py

--- Python/1 - Python3 Pt. 1/adivinhacao.py
+++ Python/1 - Python3 Pt. 1/adivinhacao.py
@@ -3,6 +3,7 @@ from random import randint
 def jogar():
 
     cores = {
+        
         'reset':'\033[m',
         'vermelho':'\033[0;31m',
         'vermelho_erro':'\033[1;31m',
@@ -10,21 +11,31 @@ def jogar():
         'verde_negrito':'\033[1;32m',
         'sublinhado':'\033[4;37m',
         'azul':'\033[0;36m',
-        'amarelo': '\033[0;33m'
+        'amarelo': '\033[0;33m',
+        'roxo': '\033[0;35m'
+        
     }
 
-    print('-~-'*10, f'-~-  {cores["azul"]}Jogo da Adivinhação!{cores["reset"]}  -~-', '-~-'*10, sep='\n', end='\n\n')
+    print('-~-' * 10, f'-~-  {cores["azul"]}Jogo da Adivinhação!{cores["reset"]}  -~-', '-~-' * 10, sep='\n', end='\n\n')
 
-    print('Qual nível de dificuldade?', f'\n\n{cores["verde"]}(1) Fácil {cores["amarelo"]}(2) Médio {cores["vermelho"]}(3) Difícil{cores["reset"]}', end='\n\n')
+    print('Qual nível de dificuldade?', f'\n\n{cores["verde"]}(1) Fácil {cores["amarelo"]}(2) Médio {cores["vermelho"]}(3) Difícil {cores["roxo"]}(4) Personalizado{cores["reset"]}', end='\n\n')
+
+    numero_maximo = 100
 
     nivel = int(input('Defina o nível: '))
     print('\n', end='')
 
     if nivel == 1: tentativas = 20
     elif nivel == 2: tentativas = 10
+    elif nivel == 3: tentativas = 5
+    elif nivel == 4:
+        
+        numero_maximo = int(input('Defina o número máximo aleatório: '))
+        tentativas = int(input('Defina o número de tentativas: '))
+        
     else: tentativas = 5
 
-    numero_secreto = randint(0, 100)
+    numero_secreto = randint(0, numero_maximo)
 
     pontos = 1000
 
@@ -32,11 +43,11 @@ def jogar():
         
         print(f'Tentativa {rodada} de {tentativas}', end='\n\n')
 
-        chute_jogador = int(input('Digite um número entre 1 e 100: '))
+        chute_jogador = int(input(f'Digite um número entre 1 e {numero_maximo}: '))
         
-        if chute_jogador < 1 or chute_jogador > 100:
+        if chute_jogador < 1 or chute_jogador > numero_maximo:
             
-            print('\n', f'{cores["vermelho_erro"]}VOCÊ DEVE DIGITAR UM NÚMERO ENTRE 1 E 100!{cores["reset"]}', sep='', end='\n\n')
+            print('\n', f'{cores["vermelho_erro"]}VOCÊ DEVE DIGITAR UM NÚMERO ENTRE 1 E {numero_maximo}!{cores["reset"]}', sep='', end='\n\n')
             continue
 
         acertou = chute_jogador == numero_secreto
