f975b584e6044bfd0c1621a3f85cde476f5355d5
Eduardo Ranucci <ffreakEd@outlook.com>
2021-12-26 13:22:51 -0300

    módulo 02 terminado

--- Python/2 - Python3 Pt. 2/forca.py
+++ Python/2 - Python3 Pt. 2/forca.py
@@ -8,7 +8,7 @@ def jogar():
         'vermelho_erro': '\033[1;31m',
         'verde': '\033[0;32m',
         'verde_negrito': '\033[1;32m',
-        'sublinhado': '\033[4;37m',
+        'sublinhado': '\033[4m',
         'azul': '\033[0;36m',
         'amarelo': '\033[0;33m'
     }
@@ -20,7 +20,14 @@ def jogar():
     acertou = False
 
     while not enforcou and not acertou:
-        pass
+
+        chute = input('Qual letra? ').strip()
+
+        i = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print(f'Encontrei a letra {cores["sublinhado"]}{chute.upper()}{cores["reset"]} na posição {i}.')
+            i += 1
     
     print('-~-'*10, f'-~-~-~-  {cores["azul"]}Fim de jogo!{cores["reset"]}  -~-~-~-', '-~-'*10, sep='\n')
     
