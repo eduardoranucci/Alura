e3ef68c7ada469b023559e28b7a80b8327ec5911
Eduardo Ranucci <ffreakEd@outlook.com>
2021-12-26 16:39:13 -0300

    curso finalizado

--- Python/2 - Python3 Pt. 2/forca.py
+++ Python/2 - Python3 Pt. 2/forca.py
@@ -1,9 +1,6 @@
 from random import randint
 
-
-def jogar():
-
-    cores = {
+cores = {
         'reset': '\033[m',
         'vermelho': '\033[0;31m',
         'verde_negrito': '\033[1;32m',
@@ -11,38 +8,25 @@ def jogar():
         'azul': '\033[0;36m'
     }
 
-    print('-~- ' * 10, f'-~-~-~-~-~  {cores["azul"]}-Jogo da Forca-{cores["reset"]}  ~-~-~-~-~-', '-~- ' * 10, sep='\n',
-          end='\n\n')
 
-    arquivo = open('palavras.txt', 'r', encoding='UTF-8')
-    palavras = []
+def jogar():
+    mostrar_msg_abertura()
 
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = randint(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
+    palavra_secreta = carrega_palavra_secreta()
 
     letras_acertadas = ['_' for letra in palavra_secreta]
+    print(letras_acertadas, end='\n\n')
+
     enforcou = False
     acertou = False
     tentativas = 6
-
-    print(letras_acertadas, end='\n\n')
 
     while not enforcou and not acertou:
 
         chute = input('Qual letra? ').strip().upper()
 
         if chute in palavra_secreta:
-            i = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[i] = letra
-                i += 1
+            verifica_chute(chute, letras_acertadas, palavra_secreta)
         else:
             tentativas -= 1
             if tentativas == 0:
@@ -57,11 +41,51 @@ def jogar():
         print(letras_acertadas, end='\n\n')
 
     if acertou:
-        print(f'{cores["verde_negrito"]}Você ganhou!{cores["reset"]}', end='\n\n')
+        mostrar_msg_vitoria()
     else:
-        print(f'{cores["vermelho"]}Você perdeu!{cores["reset"]}', end='\n\n')
+        mostrar_msg_derrota(palavra_secreta)
 
+
+def mostrar_msg_abertura():
+    print('-~- ' * 10, f'-~-~-~-~-~  {cores["azul"]}-Jogo da Forca-{cores["reset"]}  ~-~-~-~-~-', '-~- ' * 10, sep='\n',
+          end='\n\n')
+
+
+def mostrar_msg_encerramento():
     print('-~-' * 10, f'-~-~-~-  {cores["azul"]}Fim de jogo!{cores["reset"]}  -~-~-~-', '-~-' * 10, sep='\n')
+
+
+def carrega_palavra_secreta():
+    arquivo = open('palavras.txt', 'r', encoding='UTF-8')
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = randint(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
+
+
+def verifica_chute(chute, letras_acertadas, palavra_secreta):
+    i = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[i] = letra
+        i += 1
+
+
+def mostrar_msg_vitoria():
+    print(f'{cores["verde_negrito"]}Você ganhou!{cores["reset"]}')
+
+
+def mostrar_msg_derrota(palavra):
+    print(f'{cores["vermelho"]}Você perdeu!{cores["reset"]}', end='\n\n')
+    print(f'A palavra era {palavra}!')
 
 
 if __name__ == '__main__':
