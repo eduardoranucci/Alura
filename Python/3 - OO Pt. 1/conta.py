f0cd2367de98ea262c21662860a5aab163e4d741
Eduardo Ranucci <ffreakEd@outlook.com>
2022-01-10 19:54:04 -0300

    módulo 04 terminado

--- Python/3 - OO Pt. 1/conta.py
+++ Python/3 - OO Pt. 1/conta.py
@@ -1,16 +1,20 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite=1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'Saldo: R${self.saldo}, do titular {self.titular}.')
+        print(f'Saldo: R${self.__saldo}, do titular {self.__titular}.')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def tranfere(self, destino, valor):
+        self.saca(valor)
+        destino.deposita(valor)
--- Python/3 - OO Pt. 1/main.py
+++ Python/3 - OO Pt. 1/main.py
@@ -1,8 +1,13 @@
 from conta import Conta
 
 conta = Conta(117, 'Eduardo Ranucci', 100.0)
+conta2 = Conta(123, 'Nico', 200.0)
 
 conta.deposita(15.0)
 conta.extrato()
-conta.saca(115.0)
+
+print()
+
+conta2.tranfere(conta, 10.0)
 conta.extrato()
+conta2.extrato()
