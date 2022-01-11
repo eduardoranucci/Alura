e0bc56ad8086fd66a762d95c4cac9c903f716238
Eduardo Ranucci <ffreakEd@outlook.com>
2022-01-11 20:27:32 -0300

    curso terminado

--- Python/3 - OO Pt. 1/conta.py
+++ Python/3 - OO Pt. 1/conta.py
@@ -12,9 +12,44 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
-    def saca(self, valor):
-        self.__saldo -= valor
+    def __saldo_suficiente(self, valor):
+        valor_disponivel = self.__saldo + self.limite
+        return valor <= valor_disponivel
 
-    def tranfere(self, destino, valor):
+    def saca(self, valor):
+        if self.__saldo_suficiente(valor):
+            self.__saldo -= valor
+        else:
+            print('Saldo insuficiente')
+
+    def transfere(self, destino, valor):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
--- Python/3 - OO Pt. 1/main.py
+++ Python/3 - OO Pt. 1/main.py
@@ -1,13 +1,26 @@
 from conta import Conta
+from cliente import Cliente
 
 conta = Conta(117, 'Eduardo Ranucci', 100.0)
 conta2 = Conta(123, 'Nico', 200.0)
+
+cliente = Cliente('dio')
 
 conta.deposita(15.0)
 conta.extrato()
 
 print()
 
-conta2.tranfere(conta, 10.0)
+conta2.transfere(conta, 10.0)
 conta.extrato()
 conta2.extrato()
+
+conta.saldo
+
+cliente.nome = 'jojo'
+cliente.nome
+
+conta.saca(2000)
+conta.extrato()
+
+conta.codigo_banco
