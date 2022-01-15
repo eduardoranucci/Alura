33734eb25533c80294d9a63807c897de0c5ebe8b
Eduardo Ranucci <ffreakEd@outlook.com>
2022-01-15 14:50:33 -0300

    módulo 03 terminado

--- Python/4 - OO Pt. 2/modelo.py
+++ Python/4 - OO Pt. 2/modelo.py
@@ -20,6 +20,9 @@ class Programa:
     def nome(self, nome):
         self._nome = nome.title()
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Likes: {self._likes}'
+
 
 class Filme(Programa):
 
@@ -27,6 +30,9 @@ class Filme(Programa):
         
         super(Filme, self).__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Duração: {self.duracao}min - Likes: {self._likes}'
 
 
 class Serie(Programa):
@@ -36,15 +42,18 @@ class Serie(Programa):
         super(Serie, self).__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - Likes: {self._likes}'
 
-vingadores = Filme('vingadores: guerra infinita', 2018, 160)
-vingadores.dar_like()
 
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}min - Likes: {vingadores.likes}')
+matrix = Filme('matrix', 2018, 160)
+matrix.dar_like()
 
 the_office = Serie('the office', 2005, 9)
 the_office.dar_like()
 the_office.dar_like()
 
-print(f'Nome: {the_office.nome} - Ano: {the_office.ano} - '
-      f'Temporadas: {the_office.temporadas} - Likes: {the_office.likes}')
+filmes_e_series = [matrix, the_office]
+
+for programa in filmes_e_series:
+    print(programa)
