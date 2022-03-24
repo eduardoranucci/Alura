6cc5a1522175ff1d2e1511f58e6367f1331400ea
Eduardo Ranucci <ffreakEd@outlook.com>
2022-03-24 20:13:45 -0300

    Módulo 02 terminado

--- Python/5 - String/main.py
+++ Python/5 - String/main.py
@@ -1,8 +1,21 @@
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
 
-url_base = url[0:url.find('?')]
+# separa a url base e os parametros
+url_base = url[:url.find('?')]
 print(url_base)
 
 url_parametros = url[url.find('?')+1:]
 print(url_parametros)
+
+# busca um parametro
+parametro_busca = 'moedaDestino'
+indice_valor = url_parametros.find(parametro_busca) + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
