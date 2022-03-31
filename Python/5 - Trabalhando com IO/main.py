fc06ad0048dae1ec4ac1b769f1bff96b6ff9dde0
Eduardo Ranucci <ffreakEd@outlook.com>
2022-03-30 22:16:19 -0300

    Módulo 03 terminado

--- Python/5 - Trabalhando com IO/main.py
+++ Python/5 - Trabalhando com IO/main.py
@@ -1,4 +1,12 @@
-arquivo_contato = open('contatos.csv')
+try:
+    with open('contatos.csv') as arquivo_contato:
 
-for linha in arquivo_contato:
-    print(linha, end='')
+        for linha in arquivo_contato:
+            print(linha, end='')
+
+except FileNotFoundError:
+    print('Arquivo não encontrado.')
+
+except PermissionError:
+    print('Sem permissão de escrita.')
+
