115301b0618f5f223dabd8a1ff1fc6c49ccbb670
Eduardo Ranucci <ffreakEd@outlook.com>
2022-04-11 20:20:47 -0300

    Módulo 01 terminado

---
+++ Python/7 - Tratamento de erros/main.py
@@ -0,0 +1,42 @@
+from pprint import pprint
+
+
+class Cliente:
+
+    def __init__(self, nome, cpf, profissao):
+
+        self.nome = nome
+        self.cpf = cpf
+        self.profissao = profissao
+
+
+# cliente = Cliente('Dio Brando', '123.456.789-00', 'Dev')
+# pprint(cliente.__dict__, width=40)
+
+
+class ContaCorrente:
+
+    total_contas_criadas = 0
+    taxa_operacao = None
+
+    def __init__(self, cliente, agencia, numero):
+
+        self.saldo = 100
+        self.cliente = cliente
+        self.agencia = agencia
+        self.numero = numero
+
+        ContaCorrente.total_contas_criadas += 1
+        ContaCorrente.taxa_operacao = 30 / ContaCorrente.total_contas_criadas
+
+    def transferir(self, valor, favorecido):
+        favorecido.depositar(valor)
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+
+conta_corrente = ContaCorrente(None, '00', '101')
