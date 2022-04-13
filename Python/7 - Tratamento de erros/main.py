952a6d7e88ce080877d08c803a1b34462c479240
Eduardo Ranucci <ffreakEd@outlook.com>
2022-04-13 20:11:07 -0300

    Módulo 0

--- Python/7 - Tratamento de erros/main.py
+++ Python/7 - Tratamento de erros/main.py
@@ -4,7 +4,6 @@ from pprint import pprint
 class Cliente:
 
     def __init__(self, nome, cpf, profissao):
-
         self.nome = nome
         self.cpf = cpf
         self.profissao = profissao
@@ -20,14 +19,52 @@ class ContaCorrente:
     taxa_operacao = None
 
     def __init__(self, cliente, agencia, numero):
+        self.__saldo = 100
+        self.__agencia = 0
+        self.__numero = 0
 
-        self.saldo = 100
         self.cliente = cliente
-        self.agencia = agencia
-        self.numero = numero
+        self.__set_agencia(agencia)
+        self.__set_numero(numero)
 
         ContaCorrente.total_contas_criadas += 1
         ContaCorrente.taxa_operacao = 30 / ContaCorrente.total_contas_criadas
+
+    @property
+    def agencia(self):
+        return self.__agencia
+
+    def __set_agencia(self, valor):
+        if not isinstance(valor, int):
+            raise ValueError('Agencia deve ser um inteiro.', valor)
+        if valor <= 0:
+            raise ValueError('Agencia deve ser maior que zero.')
+
+        self.__agencia = valor
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    def __set_numero(self, valor):
+        if not isinstance(valor, int):
+            raise ValueError('Numero deve ser um inteiro.')
+        if valor <= 0:
+            raise ValueError('Numero deve ser maior que zero.')
+
+        self.__numero = valor
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    def __set_saldo(self, valor):
+        if not isinstance(valor, int):
+            raise ValueError('Saldo deve ser um inteiro.')
+        if valor <= 0:
+            raise ValueError('Saldo deve ser maior que zero.')
+
+        self.__saldo = valor
 
     def transferir(self, valor, favorecido):
         favorecido.depositar(valor)
@@ -39,4 +76,26 @@ class ContaCorrente:
         self.saldo += valor
 
 
-conta_corrente = ContaCorrente(None, '00', '101')
+def main():
+
+    contas = []
+
+    while True:
+        try:
+            nome = input('Nome do cliente: ')
+            agencia = int(input('Número da agência: '))
+            numero = int(input('Número da conta corrente: '))
+            cliente = Cliente(nome, None, None)
+            conta_corrente = ContaCorrente(cliente, agencia, numero)
+            contas.append(conta_corrente)
+            print()
+        except ValueError as E:
+            print(type(E.args[1]))
+            exit()
+        except KeyboardInterrupt:
+            print(f'\n\n{len(contas)}(s) contas criadas')
+            exit()
+
+
+if __name__ == '__main__':
+    main()
