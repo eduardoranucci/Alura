2536ce3e60b0bf8ee4eb33530e58abcd1555b847
eduardoranucci <ffreaked@outlook.com>
2022-08-15 20:47:25 -0300

    módulo 04 finalizado

--- Django/AluraReceitas/receitas/views.py
+++ Django/AluraReceitas/receitas/views.py
@@ -3,7 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sorvete',
+        3: 'Pizza',
+        4: 'Pudim',
+        5: 'Bolo de cenoura',
+        6: 'Nhoque'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
