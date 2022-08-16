dfe26e81cbe750157aaab27ceb55f82a2b137857
eduardoranucci <ffreaked@outlook.com>
2022-08-16 20:24:46 -0300

    módulo 03 finalizado

--- Django/AluraReceitas/receitas/admin.py
+++ Django/AluraReceitas/receitas/admin.py
@@ -4,10 +4,11 @@ from .models import Receita
 # Register your models here.
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria')
+    list_display = ('id', 'nome_receita', 'categoria', 'pessoa','publicada')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
-    list_filter = ('categoria',)
+    list_filter = ('categoria', 'publicada')
+    list_editable = ('publicada',)
     list_per_page = 10
 
 
