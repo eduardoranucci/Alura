1353c836d680cf79df3e02ebcd116f8965ba28d5
eduardoranucci <ffreaked@outlook.com>
2022-08-16 19:59:03 -0300

    módulo 02 finalizado

--- Django/AluraReceitas/receitas/models.py
+++ Django/AluraReceitas/receitas/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 # Create your models here.
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=100)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
