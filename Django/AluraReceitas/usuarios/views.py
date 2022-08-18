2fbb2ee664db4b107840e3b5a19598cfbf053f21
eduardoranucci <ffreaked@outlook.com>
2022-08-17 22:39:18 -0300

    módulo 04 finalizado

--- Django/AluraReceitas/usuarios/views.py
+++ Django/AluraReceitas/usuarios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from receitas.models import Receita
 
 # Create your views here.
 
@@ -60,7 +61,15 @@ def login(request):
 def dashboard(request):
     
     if request.user.is_authenticated:
-        return render(request, 'usuarios/dashboard.html')
+
+        id = request.user.id
+        receitas = Receita.objects.order_by('-data_receita').filter(pessoa=id)
+
+        dados = {
+            'receitas': receitas
+        }
+
+        return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
 
@@ -68,3 +77,31 @@ def logout(request):
     
     auth.logout(request)
     return redirect('index')
+
+def cria_receita(request):
+
+    if request.method == 'POST':
+        
+        nome = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+
+        receita = Receita(pessoa=user,
+                          nome_receita=nome,
+                          ingredientes=ingredientes,
+                          modo_preparo=modo_preparo,
+                          tempo_preparo=tempo_preparo,
+                          rendimento=rendimento,
+                          categoria=categoria,
+                          foto_receita=foto_receita)
+        receita.save()
+
+        return redirect('dashboard')
+
+    else:
+        return render(request, 'usuarios/cria_receita.html')
