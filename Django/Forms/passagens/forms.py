d1cbf95b73467ac99db8b21f88e018ec00fd43e9
eduardoranucci <ffreaked@outlook.com>
2022-08-20 19:50:45 -0300

    módulo 03 finalizado

--- Django/Forms/passagens/forms.py
+++ Django/Forms/passagens/forms.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from passagens.classe_viagem import tipos_de_classe
 
 class PassagemForm(forms.Form):
 
@@ -7,3 +9,12 @@ class PassagemForm(forms.Form):
     destino = forms.CharField(label='Destino', max_length=50)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today)
+    classe_viagem = forms.ChoiceField(label='Classe do vôo', choices=tipos_de_classe)
+    informacoes = forms.CharField(
+        label='Informações adicionais',
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(label='E-mail', max_length=100)
--- Django/Forms/setup/settings.py
+++ Django/Forms/setup/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'passagens',
-    'tempus_dominus'
+    'tempus_dominus',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
