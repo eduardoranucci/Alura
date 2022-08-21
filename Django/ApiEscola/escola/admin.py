b6aa1ab0d9b01d2f3c2b9340c9d17561256ba20a
eduardoranucci <ffreaked@outlook.com>
2022-08-21 19:58:18 -0300

    módulo 02 finalizado

--- Django/ApiEscola/escola/admin.py
+++ Django/ApiEscola/escola/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
 # Register your models here.
+
+class Alunos(admin.ModelAdmin):
+
+    list_display = ('id', 'nome', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'cpf')
+    list_per_page = 20
+    
+
+class Cursos(admin.ModelAdmin):
+
+    list_display = ('id', 'codigo', 'descricao', 'nivel')
+    list_display_links = ('id', 'codigo')
+    search_fields = ('descricao', 'nivel')
+    list_per_page = 20
+
+
+admin.site.register(Aluno, Alunos)
+admin.site.register(Curso, Cursos)
