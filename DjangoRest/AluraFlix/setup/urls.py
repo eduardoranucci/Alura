657823e933066a584432d450466eb52e3c1eae41
eduardoranucci <ffreakEd@outlook.com>
2022-08-27 17:05:59 -0300

    curso finalizado

--- DjangoRest/AluraFlix/setup/urls.py
+++ DjangoRest/AluraFlix/setup/urls.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from aluraflix.views import ProgramaViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="AluraFlix",
+      default_version='v1',
+      description="Provedora de filmes e séries DRF.",
+      terms_of_service="#",
+      contact=openapi.Contact(email="c3op@alura.com.br"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register('programas', ProgramaViewSet, basename='programas')
@@ -9,4 +25,6 @@ router.register('programas', ProgramaViewSet, basename='programas')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
