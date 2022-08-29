e65a705415877814a1432c888de2d4e002146012
eduardoranucci <ffreakEd@outlook.com>
2022-08-29 15:47:34 -0300

    módulo 04 finalizado

--- Python/11 - TDD/tests/test_bytebank.py
+++ Python/11 - TDD/tests/test_bytebank.py
@@ -1,4 +1,6 @@
 from codigo.bytebank import Funcionario
+import pytest
+from pytest import mark
 
 class TestClass:
     
@@ -33,3 +35,26 @@ class TestClass:
         resultado = diretor.salario
         
         assert resultado == esperado
+        
+    @mark.calcular_bonus    
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        
+        entrada = 1000
+        esperado = 100
+        
+        funcionario_teste = Funcionario('Teste', '02/02/2000', entrada)
+        resultado = funcionario_teste.calcular_bonus()
+        
+        assert esperado == resultado
+        
+    @mark.calcular_bonus 
+    def test_quando_calcular_bonus_recebe_1000000_deve_retornar_exception(self):
+        
+        with pytest.raises(Exception):
+            
+            entrada = 1000000
+            
+            funcionario_teste = Funcionario('Teste', '02/02/2000', entrada)
+            resultado = funcionario_teste.calcular_bonus()
+            
+            assert resultado
